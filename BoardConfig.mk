2dd121442f1aed03ed8584d9096fe23760284462
CertifiedBlyndGuy <rjmurdok@linux.com>
2017-06-02 06:45:52 -0600

    onyx: bc: improve dexpreopt
    *Remove the conditions
    *Preopt everything but prebuilts
    
    Change-Id: Ia74b586d4fe94f914fa98f95f74515416433b929

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -77,14 +77,9 @@ BOARD_CHARGER_DISABLE_INIT_BLANK := true
 BOARD_HARDWARE_CLASS += $(PLATFORM_PATH)/cmhw
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 
-# Enable dexpreopt to speed boot time
-ifeq ($(HOST_OS),linux)
-  ifeq ($(call match-word-in-list,$(TARGET_BUILD_VARIANT),user),true)
-    ifeq ($(WITH_DEXPREOPT_BOOT_IMG_ONLY),)
-      WITH_DEXPREOPT_BOOT_IMG_ONLY := true
-    endif
-  endif
-endif
+# Disable dex-preopt of prebuilts to save space.
+WITH_DEXPREOPT := true
+DONT_DEXPREOPT_PREBUILTS := true
 
 # Filesystem
 BOARD_FLASH_BLOCK_SIZE := 131072
