4dbc3d3c266454ea0c888e484f783d7a0933e646
CertifiedBlyndGuy <certifiedblyndguy@gmail.com>
2017-07-24 15:59:31 -0700

    onyx: Implement support for new gestures
    * Adapt configuration from the OnePlus 3
    * Kill deprecated gestures file
    
    Change-Id: I14ba50e6424867a53e7502637444a1b5fe521275

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -155,10 +155,5 @@ WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Gestures
 TARGET_GESTURES_NODE := "/proc/touchpanel/gesture_enable"
-TARGET_POWER_GESTURE_FILE := device/oneplus/onyx/power/gestures.c
-TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
-TARGET_DRAW_V_NODE := "/proc/touchpanel/flashlight_enable"
-TARGET_DRAW_O_NODE := "/proc/touchpanel/camera_enable"
-TARGET_DRAW_ARROW_LEFT_NODE := "/proc/touchpanel/music_enable"
 
 -include vendor/oneplus/onyx/BoardConfigVendor.mk
